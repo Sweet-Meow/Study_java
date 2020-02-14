1f33f0d4248624a39ecab5313d0d17fc1dfb1482
DalKkomBear <inin0119@naver.com>
2020-02-14 16:56:32 +0900

    배열 관련

--- src/study1/study1_03.java
+++ src/study1/study1_03.java
@@ -6,7 +6,7 @@ public class study1_03 {
     public static void main(String[] args) {
         String[] Group = { "김인철", "윤현아", "쪼리", "혀니"};
         for (String e : Group) {  // Group배열의 값을 변수e에 대입
-            System.out.println(e + " 출력완료");
+            System.out.println(e + " 출력");
         }
     }
 }
