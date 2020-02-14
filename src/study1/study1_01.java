aefaacb1e82b7c9f3922d966f4c1ffabe576b722
DalKkomBear <inin0119@naver.com>
2020-02-14 16:22:56 +0900

    Initial commit

---
+++ src/study1/study1_01.java
@@ -0,0 +1,14 @@
+package study1;
+// 배열생성
+
+public class study1_01 {
+
+    public static void main(String[] args) {
+        String[] classGroup = { "윤현아", "잠만보", "지각쟁이", "현보"};
+        System.out.println(classGroup[0]);
+        System.out.println(classGroup[1]);
+        System.out.println(classGroup[2]);
+        System.out.println(classGroup[3]);
+    }
+
+}
