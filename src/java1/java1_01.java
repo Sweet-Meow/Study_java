0204fa3e5d56e92360569cd0ff902d3b5310eb61
DalKkomBear <inin0119@naver.com>
2020-02-17 14:20:31 +0900

    JAVA1 학습 및 Account Application Project

---
+++ src/java1/java1_01.java
@@ -0,0 +1,24 @@
+package java1;
+// 데이터와 연산
+
+
+public class java1_01 {
+
+    public static void main(String[] args) {
+        System.out.println(6);  // 숫자(Number)
+        System.out.println("Six / 여섯");  // 문자(String)
+
+        System.out.println("6");  // 문자(String)
+
+        System.out.println(6 + 6); // 숫자연산
+        System.out.println("6" + "6");  // 문자연산
+
+        System.out.println("1111".length()); // 문자열길이
+        // .length() : 해당 문자열의 길이 출력 함수
+
+        System.out.println("Hello Java"); // 문자열(String)
+        System.out.println('H');  // 문자열(Char)
+
+
+    }
+}
