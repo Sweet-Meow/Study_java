422ec5983ac80d2697bdb3da3b7ead00beb19ccd
DalKkomBear <inin0119@naver.com>
2020-02-25 23:11:04 +0900

    자바기초 - 변수

---
+++ src/pack_java/java_01.java
@@ -0,0 +1,32 @@
+package pack_java;
+//변수 관련
+
+public class java_01 {
+    public static void main(String[] args) {
+        int x = 1;
+        // int - "정수"를 의미
+        short y = 2;
+        long z = 1;
+
+        double mydouble = 1.23;
+        // double - "소수점을 가지는 숫자"를 의미
+        float myfloat = 1.24F;
+
+        char mychar = '야';
+
+        Boolean myboolean = true;
+        // Boolean - 참(true)인지 거짓(false)인지 판별
+        Byte mybyte = 12;
+        // Byte - "용량"을 의미
+
+        System.out.println("Hello World!");
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(myboolean);
+        System.out.println(mybyte);
+        System.out.println(mychar);
+        System.out.println(mydouble);
+
+    }
+}
