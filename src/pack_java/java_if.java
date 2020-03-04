0ca1b056bad1f7cf35cbb96549666392392a36ec
DalKkomBear <inin0119@naver.com>
2020-03-04 19:55:50 +0900

    자바기초 - 전역변수

---
+++ src/pack_java/java_if.java
@@ -0,0 +1,16 @@
+package pack_java;
+
+public class java_if {
+
+    public static void main(String [] args) {
+        String name= "Object Name";
+
+        if (name == "Object Name") {
+            System.out.println("Collect");
+        } else if (name != "Objcet Name") {
+            System.out.println("Wrong Object Name");
+        }
+
+    }
+
+}
---
+++ src/study4/study4_01.java
@@ -0,0 +1,16 @@
+package study4;
+// 전역변수 & 지역변수
+
+public class study4_01 {
+    static void a()
+    {
+        int i = 0;
+    }
+
+    public static void main(String [] args) {
+        for(int i = 0; i < 5; i++) {
+            a();
+            System.out.println(i);
+        }
+    }
+}
